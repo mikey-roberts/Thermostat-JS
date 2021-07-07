b7eba0a98597f7f62c181e54a55013f86f2bf4ce
mikey-roberts <mikey_roberts@outlook.com>
2021-07-07 11:33:51 +0100

    powersave tests written

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -12,5 +12,23 @@ describe("Thermostat", function() {
   it("temperature can be decreased", function() {
     expect(thermostat.decreaseTemp).toEqual(19);
   });
-  
+  it("goes to a maximum of 25 degrees if power saving mode is on", function() {
+    (10).times.thermostat.increaseTemp    
+    expect(thermostat.tempDisplay).toEqual(30);
+    thermostat.isPowerSaveOn
+    expect(thermostat.tempDisplay).toEqual(25);
+  });
+  it("goes to a maximum of 32 degrees if power saving mode is off", function() {
+    expect(thermostat.tempDisplay).toEqual(20);
+    (15).times.thermostat.increaseTemp    
+    thermostat.isPowerSaveOn 
+    expect(thermostat.tempDisplay).toEqual(25);
+  });
+  it("minimum temperature cannot go below 10", function() {
+    (10).times.thermostat.decreaseTemp
+    expect(thermostat.tempDisplay).toEqual(10);
+    (1).times.thermostat.decreaseTemp
+    expect(thermostat.tempDisplay).toThrow('Cannot go below 10 degrees')
+    expect(thermostat.tempDisplay).toEqual(10);
+  })
 });
