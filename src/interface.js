bc8535324c10fd875934c7ef954ab31e2e7343ef
mikey-roberts <mikey_roberts@outlook.com>
2021-07-08 15:08:23 +0100

    amended index.html button naming to pick-up the function when running the event listener

--- src/interface.js
+++ src/interface.js
@@ -1,35 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const updateTemperature() = () => {
+  const updateTemperature = () => {
     document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#temperature').className = thermostat.energyUsage();
   }
-
-  const thermostat = new Thermostat();
-  updateTemperature();
-
-  document.querySelector('#temperature-up').addEventListener('click', () => {
-    thermostat.increaseTemp();
+    const thermostat = new Thermostat();
     updateTemperature();
+    document.querySelector('#temperature-up').addEventListener('click', () => {
+      thermostat.increaseTemp();
+      updateTemperature();
+    });
+    document.querySelector('#temperature-down').addEventListener('click', () => {
+      thermostat.decreaseTemp();
+      updateTemperature();
+    });
+    document.querySelector('#temperature-reset').addEventListener('click', () => {
+      thermostat.resetTemp();
+      updateTemperature();
+    });
+    document.querySelector('#powersave-on').addEventListener('click', () => {
+      thermostat.switchPowerSaveOn();
+      document.querySelector('#power-saving-status').innerText = 'on';
+      updateTemperature();
+    })
+    document.querySelector('#powersave-off').addEventListener('click', () => {
+      thermostat.switchPowerSaveOff();
+      document.querySelector('#power-saving-status').innerText = 'off';
+      updateTemperature();
+    })
   });
-
-  document.querySelector('#temperature-down').addEventListener('click', () => {
-    thermostat.decreaseTemp();
-    updateTemperature();
-  });
-
-  document.querySelector('#temperature-reset').addEventListener('click', () => {
-    thermostat.resetTemp();
-    updateTemperature();
-  });
-
-  document.querySelector('#powersaving-on').addEventListener('click', () => {
-    thermostat.switchPowerSaveOn();
-    document.querySelector('#power-saving-status').innerText = 'on';
-    updateTemperature();
-  })
-
-  document.querySelector('#powersaving-off').addEventListener('click', () => {
-    thermostat.switchPowerSaveOff();
-    document.querySelector('#power-saving-status').innerText = 'off';
-    updateTemperature();
-  })
-});
